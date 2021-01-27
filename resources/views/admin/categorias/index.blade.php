c1ff8385a57091de47126bcb19b2c380b208e278
Franco Ezequiel Beltramella <franbeltramella@hotmail.com>
2021-01-26 23:51:20 -0300

    Version 1.0

---
+++ resources/views/admin/categorias/index.blade.php
@@ -0,0 +1,91 @@
+@extends('adminlte::page')
+
+
+@section('title','Laravel APP')
+
+@section('css')
+<!-- link rel="stylesheet" href="/css/admin_custom.css"-->
+
+
+@section('content_header')
+<h1>Categorias
+    <button type="button" class="btn btn-success" data-toggle="modal"
+    data-target="#modal">
+    Crear
+</button>
+</h1>   
+@stop
+@section('content')
+<table id="categorias" class="table table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+<div class="modal" tabindex="-1" id="modal" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+
+@section('js')
+<script>
+    $(document).ready(function(){
+        $('#categorias').DataTable({
+            "order":[[3,"desc"]]
+        });
+    });
+</script>
+@stop
+
